1538134fe986da08b10b764236b43dc3fcb52dbd
maxencetiercelet <maxence.tiercelet@edu.ece.fr>
2024-03-16 13:53:55 +0100

    Nav_acceuil en cours de création-2

--- lib/ecran_acceuil.dart
+++ lib/ecran_acceuil.dart
@@ -1,79 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'navBar.dart'; // Assurez-vous que ce chemin est correct
+import 'navBar.dart'; // Assurez-vous que le chemin vers nav_bar.dart est correct
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Calcul de la taille de l'astronaute par rapport à la taille de l'écran
+    final double astronautWidth = MediaQuery.of(context).size.width * 0.4;
+    final double astronautTop = MediaQuery.of(context).size.height * 0.2;
+
     return Scaffold(
+      backgroundColor: Color(0xFF15232E), // Couleur de fond pour tout l'écran
       appBar: AppBar(
         backgroundColor: Color(0xFF15232E), // Couleur de fond de la AppBar
         elevation: 0, // Pas d'ombre
+        title: Text(
+          'Bienvenue !',
+          style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
         actions: [
-          // Ajout de l'astronaute à l'AppBar
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
+          IconButton(
+            icon: SvgPicture.asset('assets/images/heart.svg'), // Remplacez par l'icône de coeur
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: astronautTop,
+            right: 0,
             child: SvgPicture.asset(
               'assets/images/astronaut.svg', // Assurez-vous que le chemin d'accès est correct
-              width: 30,
+              width: astronautWidth,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: astronautTop + astronautWidth / 2), // Demi-hauteur de l'astronaute pour le chevauchement
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildSection(title: 'Séries populaires', context: context),
+                // Ici, insérez votre liste horizontale ou autre widget pour les séries
+                buildSection(title: 'Comics populaires', context: context),
+                // Ici, insérez votre liste horizontale ou autre widget pour les comics
+              ],
             ),
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              color: Color(0xFF15232E), // Couleur de fond pour "Bienvenue !"
-              width: double.infinity, // Prendre toute la largeur disponible
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Bienvenue !',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            // Ajout des encadrés pour les séries et comics populaires
-            buildSectionTitle('Séries populaires', Color(0xFF1E3243)),
-            // TODO: Ajoutez ici votre liste horizontale de séries
-            buildSectionTitle('Comics populaires', Color(0xFF1E3243)),
-            // TODO: Ajoutez ici votre liste horizontale de comics
-          ],
-        ),
-      ),
       bottomNavigationBar: NavBar(onItemSelected: (index) {
-        print('Item $index sélectionné');
+        // Mettez à jour l'interface utilisateur ou naviguez vers une nouvelle page
       }),
     );
   }
 
-  Widget buildSectionTitle(String title, Color backgroundColor) {
+  Widget buildSection({required String title, required BuildContext context}) {
     return Container(
-      color: backgroundColor, // Couleur de fond pour les titres des sections
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      decoration: BoxDecoration(
+        color: Color(0xFF1E3243), // Couleur de fond pour les sections
+        borderRadius: BorderRadius.circular(20), // Bords arrondis pour le conteneur
+      ),
+      margin: EdgeInsets.all(16.0), // Marge autour du conteneur
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0), // Padding à l'intérieur du conteneur
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFFFF8100), // Couleur du texte pour les titres des sections
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFFF8100),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Logique de navigation pour "Voir plus"
+                },
+                child: Text(
+                  'Voir plus',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
-          TextButton(
-            onPressed: () {
-              // Ajoutez votre logique de navigation pour "Voir plus"
-            },
-            child: Text(
-              'Voir plus',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          SizedBox(height: 10), // Espace entre le titre et le contenu de la section
+          // Ajoutez ici les widgets pour les séries ou comics, comme un CarouselSlider ou ListView.builder
         ],
       ),
     );
