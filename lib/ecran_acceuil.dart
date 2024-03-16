b6dacf13b8c7b2b53ce6285fc92378d5a4b18a91
maxencetiercelet <maxence.tiercelet@edu.ece.fr>
2024-03-16 22:20:56 +0100

    Nav_acceuil en cours de création-3

--- lib/ecran_acceuil.dart
+++ lib/ecran_acceuil.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'navBar.dart'; // Assurez-vous que le chemin vers nav_bar.dart est correct
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Calcul de la taille de l'astronaute par rapport à la taille de l'écran
-    final double astronautWidth = MediaQuery.of(context).size.width * 0.4;
-    final double astronautTop = MediaQuery.of(context).size.height * 0.2;
+    // Utilisez les dimensions et positions comme définies dans vos captures d'écran.
+    final double astronautSize = 121.85;
+    final double astronautRight = MediaQuery.of(context).size.width - astronautSize - 244; // La position X de l'astronaute doit être ajustée ici
+    final double astronautTop = 16; // La position Y de l'astronaute doit être ajustée ici
+
+    final double sectionWidth = 424; // La largeur de la section doit être ajustée ici
+    final double sectionHeight = 329; // La hauteur de la section doit être ajustée ici
+    final double sectionTop = 107; // La position Y de la section doit être ajustée ici
 
     return Scaffold(
       backgroundColor: Color(0xFF15232E), // Couleur de fond pour tout l'écran
@@ -29,22 +34,35 @@ class HomeScreen extends StatelessWidget {
         children: [
           Positioned(
             top: astronautTop,
-            right: 0,
+            right: astronautRight,
             child: SvgPicture.asset(
               'assets/images/astronaut.svg', // Assurez-vous que le chemin d'accès est correct
-              width: astronautWidth,
+              width: astronautSize,
+              height: astronautSize,
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: astronautTop + astronautWidth / 2), // Demi-hauteur de l'astronaute pour le chevauchement
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                buildSection(title: 'Séries populaires', context: context),
-                // Ici, insérez votre liste horizontale ou autre widget pour les séries
-                buildSection(title: 'Comics populaires', context: context),
-                // Ici, insérez votre liste horizontale ou autre widget pour les comics
-              ],
+            padding: EdgeInsets.only(top: sectionTop),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  buildSection(
+                      title: 'Séries populaires',
+                      width: sectionWidth,
+                      height: sectionHeight,
+                      context: context
+                  ),
+                  // Ici, insérez votre liste horizontale ou autre widget pour les séries
+                  buildSection(
+                      title: 'Comics populaires',
+                      width: sectionWidth,
+                      height: sectionHeight,
+                      context: context
+                  ),
+                  // Ici, insérez votre liste horizontale ou autre widget pour les comics
+                ],
+              ),
             ),
           ),
         ],
@@ -55,40 +73,27 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  Widget buildSection({required String title, required BuildContext context}) {
+  Widget buildSection({required String title, required double width, required double height, required BuildContext context}) {
     return Container(
+      width: width,
+      height: height,
       decoration: BoxDecoration(
         color: Color(0xFF1E3243), // Couleur de fond pour les sections
         borderRadius: BorderRadius.circular(20), // Bords arrondis pour le conteneur
       ),
-      margin: EdgeInsets.all(16.0), // Marge autour du conteneur
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0), // Padding à l'intérieur du conteneur
+      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // Marge autour du conteneur
+      padding: EdgeInsets.all(16.0), // Padding à l'intérieur du conteneur
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFFF8100),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Logique de navigation pour "Voir plus"
-                },
-                child: Text(
-                  'Voir plus',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFFF8100),
+            ),
           ),
-          SizedBox(height: 10), // Espace entre le titre et le contenu de la section
           // Ajoutez ici les widgets pour les séries ou comics, comme un CarouselSlider ou ListView.builder
         ],
       ),
